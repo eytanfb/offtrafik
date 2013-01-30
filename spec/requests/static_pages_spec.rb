2a7dfbeb2067e37024e405b7e833929b6989e82a
Eytan Anjel <anjel@usc.edu>
2013-01-30 15:53:40 -0800

    added spec/support/utilities.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,52 +6,31 @@ describe "Static pages" do
 
   describe "Home page" do
     before{ visit root_path }
-    it{ should have_selector('h1', :text => 'Sample App')}
+    it{ should have_selector('h1', text: 'Sample App')}
     it{ should have_selector('title',
-                        :text => "Koc Carpool Project | Home") }
+                        full_title('Home')) }
   
   end
 
   describe "Help page" do
-
-    it "should have the h1 'Help'" do
-      visit help_path
-      page.should have_selector('h1', :text => 'Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit help_path
-      page.should have_selector('title',
-                        :text => "Koc Carpool Project | Help")
-    end
+    before{ visit help_path }
+    it { should have_selector('h1', text: 'Help')}
+    it { should have_selector('title', full_title('Help')) }
+    
   end
 
   describe "About page" do
-
-    it "should have the h1 'About Us'" do
-      visit about_path
-      page.should have_selector('h1', :text => 'About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit about_path
-      page.should have_selector('title',
-                    :text => "Koc Carpool Project | About Us")
-    end
+    before {visit about_path }
+    it { should have_selector('h1', text: 'About Us')}
+    it { should have_selector('title', text: 'Koc Carpool Project | About Us')}
+  
   end
   
   describe "Contact page" do
-
-    it "should have the h1 'Contact'" do
-      visit contact_path
-      page.should have_selector('h1', text: 'Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      visit contact_path
-      page.should have_selector('title',
-                    text: "Koc Carpool Project | Contact")
-    end
+    before{ visit contact_path }
+    it{ should have_selector('h1', text: 'Contact')}
+    it { should have_selector('title', text: 'Koc Carpool Project | Contact')}
+   
   end
   
 end
