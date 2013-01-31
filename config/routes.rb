db6186d62d2035c84e4a14615c3f67b40c786e91
Eytan Anjel <anjel@usc.edu>
2013-01-30 16:45:44 -0800

    user added. route configured. tests passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 RelearningRails::Application.routes.draw do
   
+  match '/signup', to: 'users#new'
+
   match '/', to: 'static_pages#home'
   match '/help', to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
