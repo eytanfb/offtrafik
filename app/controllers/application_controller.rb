bba10d72e62ef14abb4c4df073d70b86a7586257
Eytan Anjel <anjel@usc.edu>
2013-02-03 09:16:20 -0800

    localization method deleted.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  include SessionsHelper
-  
-  before_filter :set_locale_from_url
-
-  private
-
-  def set_locale_from_url
-    if lang = request.env['HTTP_ACCEPT_LANGUAGE']
-      lang = lang[/^[a-z]{2}/]
-      lang = :"tr-TR" if lang == "tr"
-    end
-    I18n.locale = params[:locale] || lang || I18n.default_locale
-  end
-  
+  include SessionsHelper 
 end
