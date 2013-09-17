d910062a15a6a9cf2ff382fd3d8146d032fb4ea7
Eytan Anjel <anjel@usc.edu>
2013-09-17 10:04:05 -0700

    new posting grouped select added

--- app/assets/javascripts/postings.js
+++ app/assets/javascripts/postings.js
@@ -1,6 +1,7 @@
 jQuery(function($) {
 				
 	neighborhoods = $("#_find_posting_from_address_neighborhood").html();
+	
 	$("#from_address_district").change(function(){
 		district = $("#from_address_district :selected").text();
 		options = $(neighborhoods).filter("optgroup[label='" + district + "']");
