62eff35c085f66cda811625de0216f485876ae3e
Eytan Anjel <anjel@usc.edu>
2013-11-24 23:37:18 -0800

    removal

--- db/migrate/20130925110629_remove_column_from_postings.rb
+++
@@ -1,9 +0,0 @@
-class RemoveColumnFromPostings < ActiveRecord::Migration
-  def up
-    remove_column :postings, :latitude
-  end
-
-  def down
-    add_column :postings, :latitude, :string
-  end
-end
