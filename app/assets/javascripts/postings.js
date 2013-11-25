83913f7678e59e098af668c1121ea9350715d372
Eytan Anjel <anjel@usc.edu>
2013-11-25 09:41:57 -0800

    share posting fix

--- app/assets/javascripts/postings.js
+++ app/assets/javascripts/postings.js
@@ -35,23 +35,3 @@ function makeTimepicker(divId){
 		defaultTime: 'value'
 	});
 }
-
-// script take from twitter for the share button
-!function(d,s,id){
-	var js,fjs = d.getElementsByTagName(s)[0];
-	if(!d.getElementById(id))
-	{
-		js=d.createElement(s);
-		js.id=id;
-		js.src="https://platform.twitter.com/widgets.js";
-		fjs.parentNode.insertBefore(js,fjs);
-	}
-}(document,"script","twitter-wjs");
-
-function facebookShare(uri){
-  window.open(
-    'https://www.facebook.com/sharer/sharer.php?u='+uri, 
-    'facebook-share-dialog', 
-    'width=626,height=436'); 
-  return false;
-};
