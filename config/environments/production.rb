c2218a1cacea99100b967a17ffd3c4e697b98896
Eytan Anjel <anjel@usc.edu>
2013-11-25 02:39:56 -0800

    production change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ RelearningRails::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
   # Production Mail
-  config.action_mailer.default_url_options = { :host => 'offtrafik.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'offtrafik.com' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
