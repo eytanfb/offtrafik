0c5f7f92ea0c7afa6fe6b79461a5556940cb968e
Eytan Anjel <anjel@usc.edu>
2014-01-25 08:58:33 -0800

    fixed time and datepickers

--- app/assets/javascripts/new_posting.js
+++ app/assets/javascripts/new_posting.js
@@ -1,38 +1,38 @@
 jQuery(function($) {
 				
-	var dates = $("#posting").find(".datepicker");
-	dates.datepicker({ 
+	$(".standard-datepicker").datepicker({ 
 			dateFormat: 'dd-mm-yy', minDate: 0
-		});
+	});
 		
-		function makeTimepicker(divId){
-			$(divId).timepicker({
-				minuteStep: 5,
-				showMeridian: false,
-				defaultTime: 'value'
-			});
-		}	
+	$(".frequent-datepicker").datepicker({ 
+			dateFormat: 'dd-mm-yy', minDate: 0, maxDate: 150
+	});
+		
+	var times = $("#posting").find(".timepicker");
 	
-		makeTimepicker("#starting_time");
-		makeTimepicker("#ending_time");
+	times.timepicker({
+		minuteStep: 5,
+		showMeridian: false,
+		defaultTime: 'value'
+	});	
 		
-		$("#standard-posting .posting-button").click(function(){
-			event.preventDefault();
-			$('#standard-posting .posting-button').addClass('hidden');
-			$('#standard-posting .items').toggle(500).removeClass('hidden');
-			if($("#frequent-posting .posting-button").hasClass('hidden')){
-				$('#frequent-posting .items').hide(500).addClass('hidden');
-				$('#frequent-posting .posting-button').show(500).removeClass('hidden');	
-			}
-		});
-		
-		$("#frequent-posting .posting-button").click(function(){
-			event.preventDefault();
-			$('#frequent-posting .posting-button').addClass('hidden');
-			$('#frequent-posting .items').toggle(500).removeClass('hidden');
-			if($("#standard-posting .posting-button").hasClass('hidden')){
-				$('#standard-posting .items').hide(500).addClass('hidden');
-				$('#standard-posting .posting-button').show(500).removeClass('hidden');	
-			}
-		});
+	$("#standard-posting .posting-button").click(function(){
+		event.preventDefault();
+		$('#standard-posting .posting-button').addClass('hidden');
+		$('#standard-posting .items').toggle(500).removeClass('hidden');
+		if($("#frequent-posting .posting-button").hasClass('hidden')){
+			$('#frequent-posting .items').hide(500).addClass('hidden');
+			$('#frequent-posting .posting-button').show(500).removeClass('hidden');	
+		}
+	});
+	
+	$("#frequent-posting .posting-button").click(function(){
+		event.preventDefault();
+		$('#frequent-posting .posting-button').addClass('hidden');
+		$('#frequent-posting .items').toggle(500).removeClass('hidden');
+		if($("#standard-posting .posting-button").hasClass('hidden')){
+			$('#standard-posting .items').hide(500).addClass('hidden');
+			$('#standard-posting .posting-button').show(500).removeClass('hidden');	
+		}
+	});
 });
