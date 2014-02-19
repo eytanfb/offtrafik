32c8b3e59d8954c44a8b7f632fd59317379f641d
Eytan Anjel <anjel@usc.edu>
2014-02-19 01:43:34 -0800

    sign_up email added

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,10 @@
 class UserMailer < ActionMailer::Base
   default from: "uyelik@offtrafik.com"
   
-  
+  def welcome(user_id)
+    @user = User.find user_id
+    
+    mail to: @user.email, subject: "Offtrafik’e Hoşgeldin"
+  end
   
 end
