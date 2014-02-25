5491e75a125a66e52d7a89093fdd8b63197b2134
Eytan Anjel <anjel@usc.edu>
2014-02-25 07:32:52 -0800

    firefox jquery fix

--- app/assets/javascripts/new_posting.js
+++ app/assets/javascripts/new_posting.js
@@ -1,4 +1,4 @@
-jQuery(function($) {
+$(function() {
 				
 	$(".standard-datepicker").datepicker({ 
 			dateFormat: 'dd-mm-yy', minDate: 0
@@ -16,7 +16,7 @@ jQuery(function($) {
 		defaultTime: 'value'
 	});	
 	
-	$("#standard-posting .posting-button").click(function(){
+	$("#standard-posting .posting-button").click(function(event){
 	 		event.preventDefault();
 	 		$('#standard-posting .items').toggle(500).removeClass('hidden');
 	 		// if($("#frequent-posting .posting-button").hasClass('hidden')){
@@ -24,7 +24,7 @@ jQuery(function($) {
 	 		// }
 	 	});
 	
-	 	$("#frequent-posting .posting-button").click(function(){
+	 	$("#frequent-posting .posting-button").click(function(event){
 	 		event.preventDefault();
 	 		$('#frequent-posting .items').toggle(500).removeClass('hidden');
 	 		// if($("#standard-posting .posting-button").hasClass('hidden')){
