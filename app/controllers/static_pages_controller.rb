e7486dbca12eb0ff256ccd455e17238ae069937f
Eytan Anjel <anjel@usc.edu>
2014-02-28 03:06:42 -0800

    login fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,11 +3,6 @@
 class StaticPagesController < ApplicationController
   def home
     @posting = Posting.new params[:posting]
-#     @last_3_postings = Posting.find_by_sql("select *
-# from postings as a
-# where a.id = (select max(id) from postings where user_id = a.user_id group by user_id) 
-# order by id desc
-# limit 3")
     path = if signed_in?
       if current_user.has_showable_journeys?
         user_past_responses_path(current_user)
