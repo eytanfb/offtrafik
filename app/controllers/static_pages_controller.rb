6b98c2de81be35ab2659a6481479456c7f36019c
Eytan Anjel <anjel@usc.edu>
2014-03-03 08:37:19 -0800

    some images deleted

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,18 +1,7 @@
 # encoding: utf-8
 
 class StaticPagesController < ApplicationController
-  def home
-    @posting = Posting.new params[:posting]
-    path = if signed_in?
-      if current_user.has_showable_journeys?
-        user_past_responses_path(current_user)
-      else
-        find_posting_path
-      end
-    end  
-    redirect_to path unless path.nil?
-  end
-
+  
   def help
   end
   
