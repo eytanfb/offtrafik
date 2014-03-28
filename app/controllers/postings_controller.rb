57bedd73433d258bde02f4cdc7b18675156cb56e
Eytan Anjel <anjel@usc.edu>
2014-03-28 15:41:11 -0700

    9postings per page

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -63,7 +63,7 @@ class PostingsController < ApplicationController
 
     @postings = params[:posting].present? ? Posting.live_postings.with_from_address(Posting.format(@from_address)).with_to_address(Posting.format(@to_address)).with_driving(@driving) : Posting.live_postings
     
-    @postings = @postings.includes(:user).paginate(page: params[:page], per_page: 6, order: "date asc")
+    @postings = @postings.includes(:user).paginate(page: params[:page], per_page: 9, order: "date asc")
     
     respond_to do |format|
       format.html
@@ -83,7 +83,7 @@ class PostingsController < ApplicationController
        postings_found = Posting.live_postings if postings_found.blank?
        flash.now[:warning] = "Bulundugunuz yerden aradıgınız adrese ilan bulunamadı. Ama belki aşagıdakiler hoşunuza gider!"
      end
-    @postings = postings_found.paginate page: params[:page], per_page: 6, order: "date asc"
+    @postings = postings_found.paginate page: params[:page], per_page: 9, order: "date asc"
     @from_address = from_address
     @to_address = to_address
     @driving = driving
