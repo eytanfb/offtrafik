e32b9853f1f3cc677cd538a73f1565a1b1aed619
Eytan Anjel <anjel@usc.edu>
2014-04-21 01:53:29 -0700

    added gems for caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,8 @@ Offtrafik::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
+  config.cache_store = :dalli_store
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
@@ -45,10 +46,4 @@ Offtrafik::Application.configure do
   
   config.middleware.use('SpoofIp', '64.71.24.19')
   
-  # config.middleware.use ExceptionNotification::Rack,
-#     email: {
-#       sender_address: %{"hata" <hata@offtrafik.com>},
-#       :exception_recipients => %w{eytanfb@gmail.com}
-#     }
-    
 end
