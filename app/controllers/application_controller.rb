846429dd5562d4fb6a1894a5e2d3f3185b096295
Eytan Anjel <anjel@usc.edu>
2014-05-11 13:33:10 -0700

    back to notifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,21 +5,18 @@ class ApplicationController < ActionController::Base
   helper_method :notifications
   
   def notifications
-    @notifications = []
-#    @notifications = Rails.cache.fetch("notifications") do
-#      PostingResponse.future.includes(:posting).where(posting_id: current_user.postings).limit(3).select { |response| response.accepted.nil? } if user_signed_in?
-#    end
+     @notifications = PostingResponse.future.includes(:posting).where(posting_id: current_user.postings).limit(3).select { |response| response.accepted.nil? } if user_signed_in?
   end
 
   def get_past_responses
-#    if user_signed_in?
-#      @postings_with_past_responses = []
-#      user_postings  = current_user.unagreed_postings
-#      user_responses = current_user.accepted_past_responses
-#      @postings_with_past_responses << user_postings
-#      @postings_with_past_responses << user_responses
-#      @postings_with_past_responses.flatten!
-#    end
+    if user_signed_in?
+      @postings_with_past_responses = []
+      user_postings  = current_user.unagreed_postings
+      user_responses = current_user.accepted_past_responses
+      @postings_with_past_responses << user_postings
+      @postings_with_past_responses << user_responses
+      @postings_with_past_responses.flatten!
+    end
   end
   
   def after_sign_in_path_for(user)
